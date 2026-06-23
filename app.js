b1ebb9ebbfb82e52f20e9c8611f3784847e795f8
bynbdulla <b.markhoos@gmail.com>
2026-06-23 10:39:25 +0300

    hide other choices

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 /*-------- Constants --------------------------------*/
 const choices = document.querySelector('#choices')
+const rock = document.querySelector('#rock')
+const scissors = document.querySelector('#scissors')
+const paper = document.querySelector('#paper')
+const reset = document.querySelector('#reset')
 console.log(choices)
 
 /*-------- Variables --------------------------------*/
@@ -10,7 +14,27 @@ let userChoice = null //it's empty so we can put null or ''
 /*--------- Event Listeners -----------------------------*/
 choices.addEventListener('click', function(event){
     userChoice = event.target.id
-    console.log('userChoice: ', userChoice)
+    // we want to hide buttons if they are not the userChoice 
+    // add hidden class to paper, and scissors
+    if (userChoice == 'rock'){
+        // console.log('use choice Rock')
+        paper.classList.add('hidden')
+        scissors.classList.add('hidden')
+    }
+    else if (userChoice == 'paper'){
+        rock.classList.add('hidden')
+        scissors.classList.add('hidden')
+    }
+    else {
+        rock.classList.add('hidden')
+        paper.classList.add('hidden')
+    }
+    // console.log('User Choice: ', userChoice)
+})
+
+reset.addEventListener('click', function(){
+    paper.classList.remove('hidden')
+    scissors.classList.remove('hidden')
 })
 
 /*--------- Functions --------------------------------*/
